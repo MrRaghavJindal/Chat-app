ff99a796ffec6a5653920c0d0a7a7045052a2c86
Raghav Jindal <raghavjindal345@gmail.com>
2025-07-03 12:36:17 +0530

    project

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://chatty-peach-nine.vercel.app/api",
+  withCredentials: true,
+});
