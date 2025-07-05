0ef15f6d167fd4af76923679abae50d2f937e048
Raghav Jindal <raghavjindal345@gmail.com>
2025-07-05 21:27:51 +0530

    fixed

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,6 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 // const __dirname = path.resolve();
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-
-app.use(cookieParser());
 app.use(
   cors({
     // origin: "http://localhost:5173",
@@ -27,6 +22,11 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: ["http://localhost:5173"],
-    origin: ["https://chat-app-psi-two-27.vercel.app/"],
+    origin: ["https://chat-app-psi-two-27.vercel.app"],
   },
 });
 
