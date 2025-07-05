170dfcbc678c31daeee2fb2ece2f8ffdb2faaeca
Raghav Jindal <raghavjindal345@gmail.com>
2025-07-05 18:52:50 +0530

    credentials update

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -8,9 +8,17 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    // sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    sameSite: "None", // cross-site requests supported
     secure: process.env.NODE_ENV !== "development",
   });
+
+  res.cookie("jwt", token, {
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+ 
+});
+
 
   return token;
 };
